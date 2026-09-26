701f0d7363322272ba588d827566a96b1f0dd644
Shuaibu Umar <shuaibuumar08065@gmail.com>
2026-09-26 20:37:08 +0100

    fix upgrade/buy.html show 30 days

--- contract/models.py
+++ contract/models.py
@@ -1,15 +1,7 @@
-import sqlite3
-
-DATABASE = "users.db"
-
+from database import get_conn
 
 def connect():
-
-    conn = sqlite3.connect(DATABASE)
-    conn.row_factory = sqlite3.Row
-
-    return conn
-
+    return get_conn()
 
 def get_contract_plans():
 
